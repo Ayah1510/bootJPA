20bb46362c7da3bbe289c85f54874f4cca585097
Ayah1510 <ayahhamdan@hotmail.com>
2022-07-19 16:57:45 +0300

    Using Query Methods

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(100, 'Zyah', 'Java');
+insert into alien values(101, 'Jamal', 'Android');
+insert into alien values(102, 'Jameel', 'Python');
+insert into alien values(103, 'Kamal', 'Java');
+insert into alien values(104, 'Kameel', 'IOS');
