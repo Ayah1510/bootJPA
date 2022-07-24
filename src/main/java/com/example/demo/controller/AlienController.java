edb3306c3b84f70a9876368583b57543868b1c1b
Ayah1510 <ayahhamdan@hotmail.com>
2022-07-24 12:16:37 +0300

    Final changes

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -1,17 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 
 	@Autowired
@@ -41,19 +50,45 @@ public class AlienController {
 		return mv;
 	}
 	
-	@RequestMapping("/aliens")
-	// bc the return is actual data not the viewpoint
+	@RequestMapping(path="/aliens", produces= {"application/xml"}) //to restrict requests to only xml type request
+	// because the return is actual data not the viewpoint
 	@ResponseBody 
-	public String getAliens() {
-		return repo.findAll().toString();
+	public List<Alien> getAliens() {
+		return repo.findAll();
 	}
-	
 	
 	@RequestMapping("/alien/{aid}")
-	// bc the return is actual data not the viewpoint
+	// because the return is actual data not the viewpoint
 	@ResponseBody 
-	public String getAlienbyID(@PathVariable("aid") int aid) {
-		return repo.findById(aid).toString();
+	public Optional<Alien> getAlienbyID(@PathVariable("aid") int aid) {
+		return repo.findById(aid);
 	}
+	
+	// to add new data from client (Postman)
+	@PostMapping("/alien")
+	public Alien addClientAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	
+	@GetMapping("/alien")
+	public List<Alien> getClientAliens() {
+		return repo.findAll();
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "Deleted.";
+	}
+	
+	@PutMapping("/alien")
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	
 
 }
